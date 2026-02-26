7064bcb24c84d0536a660b79cf5b313b574de786
beorht <ben.tn2002@mail.ru>
2026-02-26 13:14:08 +0500

    Update data from project

--- book/searcher-c2a407aa.js
+++ book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-251277ce.js',
+                path_to_root + 'searchindex-e3ce0315.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
